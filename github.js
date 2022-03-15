f258d920418b541ac6e06b14c15381fdeda3731e
jonathan6242 <89970273+jonathan6242@users.noreply.github.com>
2022-03-15 12:37:42 +1100

    Add files via upload

--- github.js
+++ github.js
@@ -2,21 +2,13 @@ class GitHub {
     constructor() {
         this.repo_count = 5;
         this.repos_sort = 'created: asc';
-        this.token = "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";
+        this.client_id = "aabf08f0db448770a044";
+        this.client_secret = "621acc6f7fac8dfc9152c5a39308eb63f0ba729c";
     }
     async getUser(name) {
-        const res = await fetch(`https://api.github.com/users/${name}`,
-        {
-            headers: {
-                authorization: `token ${this.token}`
-            }
-        })
-        const repoRes = await fetch(`https://api.github.com/users/${name}/repos?&per_page=${this.repo_count}&sort=${this.repos_sort}`,
-        {
-            headers: {
-                authorization: `token ${this.token}`
-            }
-        })
+        const res = await fetch(`https://api.github.com/users/${name}?client_id=${this.client_id}&client_secret=${this.client_secret}`)
+        const repoRes = await fetch(`https://api.github.com/users/${name}/repos?&per_page=${this.repo_count}&sort=${this.repos_sort}
+        &client_id=${this.client_id}&client_secret=${this.client_secret}`)
         const profileData = await res.json();
         const repoData = await repoRes.json();
         return {
