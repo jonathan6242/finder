b97eba443e6aff420bca1bbf312d578eeee788f4
jonathan6242 <89970273+jonathan6242@users.noreply.github.com>
2022-03-15 13:30:37 +1100

    Update github.js

--- github.js
+++ github.js
@@ -2,15 +2,24 @@ class GitHub {
     constructor() {
         this.repo_count = 5;
         this.repos_sort = 'created: asc';
-        this.client_id = "aabf08f0db448770a044";
-        this.client_secret = "621acc6f7fac8dfc9152c5a39308eb63f0ba729c";
     }
     async getUser(name) {
-        const res = await fetch(`https://api.github.com/users/${name}?client_id=${this.client_id}&client_secret=${this.client_secret}`)
-        const repoRes = await fetch(`https://api.github.com/users/${name}/repos?&per_page=${this.repo_count}&sort=${this.repos_sort}
-        &client_id=${this.client_id}&client_secret=${this.client_secret}`)
+        document.body.classList.add('profile-loading');
+        const res = await fetch(`https://api.github.com/users/${name}`, 
+        {
+            headers: {
+                authorization: `token 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`
+            }
+        })
+        const repoRes = await fetch(`https://api.github.com/users/${name}/repos?&per_page=${this.repo_count}&sort=${this.repos_sort}`,
+        {
+            headers: {
+                authorization: `token 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`
+            }
+        })
         const profileData = await res.json();
         const repoData = await repoRes.json();
+        document.body.classList.remove('profile-loading');
         return {
             profileData,
             repoData
